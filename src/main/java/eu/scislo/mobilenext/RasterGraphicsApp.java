c5e30c0df33cf4159f253bfc182eab2403aceff1
Tomasz Ścisło <scislot@gmail.com>
2019-01-12 21:46:19 +0100

    default image

--- src/main/java/eu/scislo/mobilenext/RasterGraphicsApp.java
+++ src/main/java/eu/scislo/mobilenext/RasterGraphicsApp.java
@@ -1,6 +1,7 @@
 package eu.scislo.mobilenext;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
@@ -9,54 +10,84 @@ import javafx.scene.image.Image;
 import javafx.scene.image.PixelReader;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import java.io.FileInputStream;
 
 public class RasterGraphicsApp extends Application {
 
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Group rootPane = new Group();
-        Scene scene = new Scene(rootPane, 512, 256);
+//        Group rootPane = new Group();
+//        Scene scene = new Scene(rootPane, 512, 256);
+//
+//        Canvas canvas = new Canvas(512, 256);
+//        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+//
+//
+//        Image image = new Image("lenna256px.png");
+//
+//
+//        graphicsContext.drawImage(image, 0, 0);
+//
+//        int heigth = (int) image.getHeight();
+//        int width = (int) image.getWidth();
+//
+//        PixelReader reader = image.getPixelReader();
+//        WritableImage writableImage = new WritableImage(heigth, width);
+//
+//        PixelWriter pixelWriter = writableImage.getPixelWriter();
+//
+//        for (int x = 0; x < width; x++) {
+//            for (int y = 0; y < heigth; y++) {
+//                Color color = reader.getColor(x, y);
+//                pixelWriter.setColor(x, y, Color.color(
+//                        color.getRed() / 3,
+//                        color.getGreen() / 3,
+//                        color.getBlue() / 3
+//                ));
+//            }
+//        }
+//
+//        graphicsContext.drawImage(writableImage, 256, 0);
+//
+//
+//
+//        rootPane.getChildren().add(canvas);
+//        primaryStage.setScene(scene);
+//        primaryStage.show();
 
-        Canvas canvas = new Canvas(512, 256);
+
+        Canvas canvas = new Canvas(256, 256);
+        VBox canvasBox = new VBox();
+        canvasBox.getChildren().add(canvas);
+
+        canvasBox.getStyleClass().add("source-image");
         GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
-
-
-        System.out.print(getClass());
         Image image = new Image("lenna256px.png");
-
-
         graphicsContext.drawImage(image, 0, 0);
 
-        int heigth = (int) image.getHeight();
-        int width = (int) image.getWidth();
+        VBox rightVBox = new VBox();
+        rightVBox.getStyleClass().add("right-pane");
+        rightVBox.setAlignment(Pos.TOP_LEFT);
+//        rightVBox.getChildren().addAll(operationLabel, code, decode);
 
-        PixelReader reader = image.getPixelReader();
-        WritableImage writableImage = new WritableImage(heigth, width);
+        VBox leftVBox = new VBox();
+        leftVBox.getStyleClass().add("left-pane");
+        leftVBox.getChildren().addAll(canvasBox);
 
-        PixelWriter pixelWriter = writableImage.getPixelWriter();
-
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < heigth; y++) {
-                Color color = reader.getColor(x, y);
-                pixelWriter.setColor(x, y, Color.color(
-                        color.getRed() / 3,
-                        color.getGreen() / 3,
-                        color.getBlue() / 3
-                ));
-            }
-        }
-
-        graphicsContext.drawImage(writableImage, 256, 0);
-
-        rootPane.getChildren().add(canvas);
-        primaryStage.setTitle("Hello World");
+        BorderPane rootPane = new BorderPane();
+        Scene scene = new Scene(rootPane, 450, 296);
+        scene.getStylesheets().add("application.css");
+        primaryStage.setTitle("Run Length Encoding");
         primaryStage.setScene(scene);
+
         primaryStage.show();
+        rootPane.setRight(rightVBox);
+        rootPane.setLeft(leftVBox);
 
     }
 
